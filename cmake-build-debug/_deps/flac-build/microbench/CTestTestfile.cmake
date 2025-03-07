d564dc7b593b5545d8e9331cdf61d1a754b1ae38
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2025-03-07 14:08:47 +0000

    SFML initial setup

---
+++ cmake-build-debug/_deps/flac-build/microbench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/Projects/CPP/fractal-generator/cmake-build-debug/_deps/flac-src/microbench
+# Build directory: C:/dev/Projects/CPP/fractal-generator/cmake-build-debug/_deps/flac-build/microbench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cmake-build-debug/_deps/flac-build/src/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/Projects/CPP/fractal-generator/cmake-build-debug/_deps/flac-src/src
+# Build directory: C:/dev/Projects/CPP/fractal-generator/cmake-build-debug/_deps/flac-build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("libFLAC")
