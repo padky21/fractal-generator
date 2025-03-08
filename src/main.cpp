2d5ea68ceb121afd0f31ce775f33d5e93d82c3a7
Vasco Filipe Teles Teixeira <61906994+padky21@users.noreply.github.com>
2025-03-08 11:53:03 +0000

    Infinite random circles

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "Fractal Generation");
     window.setFramerateLimit(144);
+    std::vector<sf::CircleShape> circles;
+    std::srand(std::time(nullptr));
 
     while (window.isOpen())
     {
@@ -14,8 +16,23 @@ int main()
                 window.close();
             }
         }
+        
+        sf::CircleShape circle(10); // Radius of 10
+        sf::Color color(rand() % 256, rand() % 256, rand() % 256);
+        circle.setFillColor(color);
+        sf::Vector2f position(rand() % 800, rand() % 600);
+        circle.setPosition(position);
 
+        circles.push_back(circle); // Store the circle
+
+        // Clear window before drawing new frame
         window.clear();
+
+        // Draw all stored circles
+        for (const auto& c : circles)
+        {
+            window.draw(c);
+        }
         window.display();
     }
 }
